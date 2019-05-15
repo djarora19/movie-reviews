f6543d3129cbc1b2b4186ee8ff2f20805ef7e4b3
Divyangkumar Arora <divyangkumar.arora@gmail.com>
2019-05-15 18:46:14 +0530

    Added API to understand REST Call using web config.

--- movie-catalogue-service/src/main/java/adj/learn/springboot/moviecatalogue/MovieCatalogueServiceApplication.java
+++ movie-catalogue-service/src/main/java/adj/learn/springboot/moviecatalogue/MovieCatalogueServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MovieCatalogueServiceApplication
@@ -13,6 +14,12 @@ public class MovieCatalogueServiceApplication
 	RestTemplate getRestTemplate()
 	{
 		return new RestTemplate();
+	}
+
+	@Bean
+	WebClient.Builder getWebClient()
+	{
+		return WebClient.builder();
 	}
 
 	public static void main(String[] args)
--- movie-catalogue-service/src/main/java/adj/learn/springboot/moviecatalogue/controller/MovieCatalogueController.java
+++ movie-catalogue-service/src/main/java/adj/learn/springboot/moviecatalogue/controller/MovieCatalogueController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import adj.learn.springboot.moviecatalogue.model.CatalogItem;
 import adj.learn.springboot.moviecatalogue.model.Movie;
@@ -19,9 +20,11 @@ import adj.learn.springboot.moviecatalogue.model.Rating;
 @RequestMapping ("/catalog")
 public class MovieCatalogueController
 {
-
 	@Autowired
 	private RestTemplate restTemplate;
+
+	@Autowired
+	private WebClient.Builder builder;
 
 	@GetMapping ("/{userId}")
 	public List<CatalogItem> getCatalogs(@PathVariable ("userId") String userId)
@@ -37,4 +40,20 @@ public class MovieCatalogueController
 			}).collect(Collectors.toList());
 	}
 
+	@GetMapping ("webclient/{userId}")
+	public List<CatalogItem> getCatalogsWebClient(@PathVariable ("userId") String userId)
+	{
+		List<Rating> ratingList = Arrays.asList(new Rating("m1", 5), new Rating("m2", 4));
+
+		return ratingList.stream().map(rating ->
+			{
+				// Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" +
+				// rating.getMovieId(),Movie.class);
+
+				Movie movie = builder.build().get().uri("http://localhost:8082/movies/" + rating.getMovieId())
+						.retrieve().bodyToMono(Movie.class).block();
+
+				return new CatalogItem(movie.getName(), "Inspirational movie", rating.getRating());
+			}).collect(Collectors.toList());
+	}
 }
