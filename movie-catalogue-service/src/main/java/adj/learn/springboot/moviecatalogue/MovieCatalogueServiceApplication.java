a93d4c4a320270bf7c759e1f55bd8f30fc2493d2
djarora19 <divyangkumar.arora@gmail.com>
2019-12-30 21:29:52 +0530

    Enabled Hystrix Dashboard

--- movie-catalogue-service/src/main/java/adj/learn/springboot/moviecatalogue/MovieCatalogueServiceApplication.java
+++ movie-catalogue-service/src/main/java/adj/learn/springboot/moviecatalogue/MovieCatalogueServiceApplication.java
@@ -5,14 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class MovieCatalogueServiceApplication
 {
 
@@ -20,9 +21,12 @@ public class MovieCatalogueServiceApplication
 	@LoadBalanced
 	RestTemplate getRestTemplate()
 	{
-		HttpComponentsClientHttpRequestFactory clientRequestFactory = new HttpComponentsClientHttpRequestFactory();
-		clientRequestFactory.setConnectTimeout(3000);
-		return new RestTemplate(clientRequestFactory);
+		/*
+		 * HttpComponentsClientHttpRequestFactory clientRequestFactory = new
+		 * HttpComponentsClientHttpRequestFactory();
+		 * clientRequestFactory.setConnectTimeout(3000);
+		 */
+		return new RestTemplate();
 	}
 
 	@Bean
